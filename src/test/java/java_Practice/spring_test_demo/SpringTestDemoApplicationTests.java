6e59c5e393378122f27ae732070a5738ea4b8102
mpavanka <pavankalyan68335@gmail.com>
2025-07-07 11:55:02 +0530

    added files

---
+++ src/test/java/java_Practice/spring_test_demo/SpringTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package java_Practice.spring_test_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
